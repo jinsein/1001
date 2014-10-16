fa5df11e192329475fdc63627672f436dca09a82
Youngwook Yoo <yoo.youngwook@gmail.com>
2014-10-16 19:59:29 -0400

    Take this

--- SYAndroidApp/src/com/example/syandroidapp/MainActivity.java
+++ SYAndroidApp/src/com/example/syandroidapp/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.syandroidapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +24,40 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        String printThis = "Wrong!";
+        try {
+	        HttpClient client = new DefaultHttpClient();
+			HttpPost request = new HttpPost("http://10.4.3.109:9090/rest/symessage");
+			
+			HttpResponse response;
+			
+			Log.e("MAINACTIVITY", "Before execution...");
+			response = client.execute(request);
+			Log.e("MAINACTIVITY", "After execution...");
+			BufferedReader rd = new BufferedReader(
+	                new InputStreamReader(response.getEntity().getContent()));
+			
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+				//System.out.println(line);
+			}
+			
+			printThis = result.toString();
+			
+			Log.e("MAINACTIVITY", printThis);
+		} catch (ClientProtocolException e) {
+			Log.e("MAINACTIVITY", e.getMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        TextView greeting = (TextView) findViewById(R.id.greetingMessage);
+        greeting.setText(printThis);
     }
 
 
