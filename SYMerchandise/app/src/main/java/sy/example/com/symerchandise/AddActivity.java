f31e963d7516ef8862ab2815b6c1346071575af6
jinsein <sjin@architech.ca>
2015-02-26 19:17:09 -0500

    android app

---
+++ SYMerchandise/app/src/main/java/sy/example/com/symerchandise/AddActivity.java
@@ -0,0 +1,29 @@
+package sy.example.com.symerchandise;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by JJIN on 15-01-28.
+ */
+public class AddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        Button b = (Button) findViewById(R.id.but_newProduct);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), NewItemActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
---
+++ SYMerchandise/app/src/main/java/sy/example/com/symerchandise/NewItemActivity.java
@@ -0,0 +1,17 @@
+package sy.example.com.symerchandise;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JJIN on 15-01-28.
+ */
+public class NewItemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newitem);
+    }
+
+}
